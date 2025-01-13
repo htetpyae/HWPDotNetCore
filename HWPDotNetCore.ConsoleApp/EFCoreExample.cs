9871fac2af8839a1340c2e4e506ba9d0fef9de6c
User <User@DESKTOP-OO7LMSC>
2025-01-13 17:51:02 +0630

    feat: EfCore CRUD

---
+++ HWPDotNetCore.ConsoleApp/EFCoreExample.cs
@@ -0,0 +1,106 @@
+﻿using HWPDotNetCore.ConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWPDotNetCore.ConsoleApp
+{
+    public class EFCoreExample
+    {
+        public void Read()
+        {
+            AppDbContext db = new AppDbContext();
+            var lst = db.Blogs.Where(x => x.DeleteFlag == false).ToList();
+
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            BlogDataModel blog = new BlogDataModel
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+
+            };
+
+            AppDbContext db = new AppDbContext();
+            db.Blogs.Add(blog);
+            var result = db.SaveChanges();
+
+            Console.WriteLine(result == 1 ? "saving success" : "failed");
+        }
+
+        public void Edit(int id)
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.Where(x => x.BlogId == id).FirstOrDefault();
+            if (item == null)
+            {
+                Console.WriteLine("no data found");
+                return;
+            }
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+        }
+
+
+        public void Update(int id, string title, string author, string content)
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.AsNoTracking().Where(x => x.BlogId == id).FirstOrDefault();
+            if(item == null)
+            {
+                Console.WriteLine("no data found");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                item.BlogTitle = title;
+            }
+
+            if (!string.IsNullOrEmpty(author))
+            {
+                item.BlogAuthor = author;
+            }
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                item.BlogContent = content;
+            }
+
+            db.Entry(item).State = EntityState.Modified; //AskNoTracking 
+            var result = db.SaveChanges();
+            Console.WriteLine(result ==  1 ? "successs" : "fail");
+        }
+
+        public void Delete(int id)
+        {
+            AppDbContext db = new AppDbContext();
+           var item = db.Blogs.AsNoTracking().Where(x => x.BlogId == id).FirstOrDefault();
+            if(item == null)
+            {
+                Console.WriteLine("no data found");
+                return;
+            }
+            db.Entry(item).State = EntityState.Deleted;
+            var result = db.SaveChanges();
+            Console.WriteLine(result == 1 ? "successs" : "fail");
+        }
+    }
+}
+
