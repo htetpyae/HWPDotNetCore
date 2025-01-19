62718d4a9d33988f58b974a2149fba6f56949ab9
User <User@DESKTOP-OO7LMSC>
2025-01-19 12:49:35 +0630

    feat: asp.net  core web api (crud)

---
+++ HWPDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,103 @@
+﻿using HWPDotNetCore.Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HWPDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _db.TblBlogs.AsNoTracking().ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EditBlogs(int id)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlogs(TblBlog blog)
+        {
+            _db.TblBlogs.Add(blog);
+            _db.SaveChanges();
+            return Ok(blog);
+        }
+
+        [HttpPut ("{id}")]
+        public IActionResult UpdateBlogs(int id, TblBlog blog)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x=> x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return Ok(item);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlogs(int id, TblBlog blog)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+             _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return Ok(item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlogs(int id)
+        {
+            var item = _db.TblBlogs.AsNoTracking().
+                Where(x => x.DeleteFlag == false).   //df ko true htrr yin use
+                FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            item.DeleteFlag = true;
+            _db.Entry(item).State = EntityState.Modified;
+            // _db.Entry(item).State= EntityState.Deleted;
+            _db.SaveChanges();
+            return Ok(item);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 aaa
 hflahkjfeuh
 ajlfglsak
+
+dotnet ef dbcontext scaffold "Server=.;Database=DotNetDB;User Id=sa;Password=sa@123;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models -c AppDbContext -f
